85b9d30fe9031fdf7d79a086fff2cff7ef3c38db
Aayushma Dhital Chhetri <aayushma012456@gmail.com>
2026-05-31 17:01:35 +0545

    Functions

---
+++ Day5/p3.py
@@ -0,0 +1,7 @@
+def message(number):
+    print("Entered number is :", number)
+
+
+a = int(input("Enter your number "))
+message(a)
+
---
+++ Day5/p4.py
@@ -0,0 +1,7 @@
+def message(what, number):
+    print("Entered", what, "number is", number)
+
+message("telephone", 11)
+message("price", 5)
+message("number", "number")
+
