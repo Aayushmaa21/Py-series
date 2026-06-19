63cc4a3573e1ca2060bd7e1f4aacf43772df679b
Aayushma Dhital Chhetri <aayushma012456@gmail.com>
2026-06-19 23:03:05 +0545

    While

---
+++ Google/loop/while/P1.py
@@ -0,0 +1,30 @@
+def attempts(n):
+    x = 1
+    while x <= n:
+        print("Attempt " + str(x))
+        x += 1
+    print("Done")
+
+attempts(5)
+
+
+
+my_variable = 5
+while my_variable < 10:
+    print("Hello")
+    my_variable += 1
+
+
+x = 1
+sum = 0
+while x < 10:
+    sum = sum + x
+    x = x + 1
+
+product = 1
+while x < 10:
+    product = product * x
+    x = x + 1
+
+print(sum, product)
+#Ouput 45 1
