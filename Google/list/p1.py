2a7118357395bd0b03225920a38bd33fa36b25ef
Aayushma Dhital Chhetri <aayushma012456@gmail.com>
2026-06-24 19:23:05 +0545

    List

---
+++ Google/list/p1.py
@@ -0,0 +1,27 @@
+fruits = ["Pineapple", "Banana", "Apple", "Melon"]
+fruits.append("Kiwi")
+print(fruits)
+
+
+fruits = ["Pineapple", "Banana", "Apple", "Melon"]
+fruits.insert(0, "Orange")
+print(fruits)
+
+fruits = ["Pineapple", "Banana", "Apple", "Melon"]
+fruits.insert(0, "Orange")
+fruits.insert(25, "Peach")
+print(fruits)
+
+
+fruits = ["Pineapple", "Banana", "Apple", "Melon"]
+fruits.insert(0, "Orange")
+fruits.insert(25, "Peach")
+fruits.pop(3)
+fruits.remove("Melon")
+fruits[2] = "Strawberry"
+print(fruits)
+
+
+
+
+
