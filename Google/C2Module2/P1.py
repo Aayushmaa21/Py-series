41938ac35e9abfecbfff4f4e1bd6c8aef6166f86
Aayushma Dhital Chhetri <aayushma012456@gmail.com>
2026-06-30 19:30:17 +0545

    File path

--- Google/C2Module2/P1.py
+++ Google/C2Module2/P1.py
@@ -4,5 +4,5 @@ print(file.readline()) #READS 2ND LINE
 print(file.read()) #READS ALL FILE FROM CURRENT POSOTION
 file.close()
 with open("spider.txt") as file: #"WITH" AUTOMATICALLY CLOSES THE FILE
-    print(file.readline()) #READS 1ST LINE CAUSE WE HAVE ALREADY CLOSED PREVIOUS OPENING
-    
+   # print(file.readline()) #READS 1ST LINE CAUSE WE HAVE ALREADY CLOSED PREVIOUS OPENING
+    print(file.read()) 
