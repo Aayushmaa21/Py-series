9a7adfd58c2bc306641c54c9e30bb25ab71bfd31
Aayushma Dhital Chhetri <aayushma012456@gmail.com>
2026-07-01 19:27:39 +0545

    Filetime

--- Google/C2Module2/f3.py
+++ Google/C2Module2/f3.py
@@ -6,4 +6,11 @@ print(a)
 #To check when file was last modified
 b = os.path.getmtime("aayu.txt")
 print(b) #Represents no of sec from 1970
+#To solve it
+import datetime
+timestamp = os.path.getmtime("aayu.txt")
+print(datetime.datetime.fromtimestamp(timestamp))
+# we are using fromtimestamp from datetime class and date time module
+#for my understanding --> precedence = R to L
 
+#Os.path module takes the info provided by operating system
